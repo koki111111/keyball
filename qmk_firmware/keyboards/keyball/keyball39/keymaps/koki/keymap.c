64ebbdf29f8bb224576e8e37a3b7dad37c2f645e
koki111111 <enomoto.kouki.1985@gmail.com>
2025-03-16 19:24:59 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/koki/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/koki/keymap.c
@@ -75,15 +75,15 @@ const uint16_t PROGMEM test_combo2[] = {KC_Y, KC_P, COMBO_END};
 const uint16_t PROGMEM test_combo3[] = {KC_Q, KC_L, COMBO_END};
 const uint16_t PROGMEM test_combo4[] = {KC_X, KC_C, COMBO_END}; 
 const uint16_t PROGMEM test_combo5[] = {KC_M, KC_J, COMBO_END};
-const uint16_t PROGMEM test_combo6[] = {KC_O, KC_MINS, COMBO_END}; 
-const uint16_t PROGMEM test_combo7[] = {KC_K, KC_T, COMBO_END}; 
+//const uint16_t PROGMEM test_combo6[] = {KC_O, KC_MINS, COMBO_END}; 
+//const uint16_t PROGMEM test_combo7[] = {KC_K, KC_T, COMBO_END}; 
 
 combo_t key_combos[] = {
     COMBO(test_combo1, KC_TAB),
     COMBO(test_combo2, KC_DEL), 
     COMBO(test_combo3, KC_ESC), 
-    COMBO(test_combo4, KC_INT5),
-    COMBO(test_combo5, KC_INT4),
-    COMBO(test_combo6, KC_LNG2),
-    COMBO(test_combo7, KC_LNG1),
+    //COMBO(test_combo4, KC_INT5),
+    //COMBO(test_combo5, KC_INT4),
+    COMBO(test_combo4, KC_LNG2),
+    COMBO(test_combo5, KC_LNG1),
 };
